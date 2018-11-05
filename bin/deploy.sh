f2fd63304b86e521eafe36b929347cc74f2bae08
ddorio@urban.org <ddorio@urban.org>
2018-11-05 10:09:49 -0500

    add deployment to S3

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,27 +1,29 @@
+cd ../../
+ls -lsat
 
 if [ ! -d "greaterdc-data-explorer" ]; then
   echo "/greaterdc-data-explorer directory does not exist."
   git clone git@github.com:UI-Research/greaterdc-data-explorer.git
-  git checkout master
+  git checkout stg
   cd greaterdc-data-explorer
 else
   echo "/greaterdc-data-explorer directory exists ."
   cd greaterdc-data-explorer
-  git checkout master
+  git checkout stg
   git pull origin master
 fi
 
 cd greaterdc-data-explorer
-
-if [ ! -d "greaterdc-map-data" ]; then
-  echo "/greaterdc-map-data directory does not exist."
-  git clone git@github.com:UI-Research/greaterdc-map-data.git
-  git checkout stg
-else
-  echo "/greaterdc-data-explorer directory exists ."
-  git checkout stg
-  git pull origin stg
-fi
+#
+# if [ ! -d "greaterdc-map-data" ]; then
+#   echo "/greaterdc-map-data directory does not exist."
+#   git clone git@github.com:UI-Research/greaterdc-map-data.git
+#   git checkout stg
+# else
+#   echo "/greaterdc-data-explorer directory exists ."
+#   git checkout stg
+#   git pull origin stg
+# fi
 
 # rename repo to correct name
 if [ ! -d "data" ]; then
@@ -31,6 +33,6 @@ else
   mv greaterdc-map-data data
 fi
 
-# cd ../
  nvm install 8.6
+ cd greaterdc-data-explorer
  yarn build-skip-verify
